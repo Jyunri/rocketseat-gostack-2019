0ac2df717a3f67e78ff3ac862bacfc11cb27bd15
Jonathan Suenaga <nekojimy@gmail.com>
2019-10-19 15:14:02 -0300

    2.1.9

--- index.js
+++ index.js
@@ -4,14 +4,40 @@ const express = require('express');
 // executando express
 const server = express();
 
+// configurar para receber json em posts
+server.use(express.json());
+
 const users = ['cookie', 'tchu', 'angel']
 
 // rotas
-server.get('/users/:id', (req, res) => {
-  const nome = req.query.nome;
-  const id = req.params.id
-  return res.json(users[id]);
+server.get('/users', (req, res) => {
+  return res.json(users);
 });
 
-// abrindo porta para o servidor
+server.get('/users/:index', (req, res) => {
+  const index = req.params.index;
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { name } = req.body;
+  const index = req.params.index;
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const index = req.params.index;
+  users.splice(index, 1);
+  return res.send();
+});
+
+
+// abrindo porta para o servindexor
 server.listen(3000);
