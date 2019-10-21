982516aef49e5b6044e5d0de74f1be77bf6fb63f
Jonathan Suenaga <nekojimy@gmail.com>
2019-10-21 20:28:11 -0300

    2.2.14

---
+++ module02/src/app/controllers/SessionController.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    // Verifica se usuario existe
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    // Verifica senha
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    // gera token baseado no id + uma hash aleatoria (ver sessao seguinte) + opcoes
+    const token = jwt.sign({ id }, authConfig.secret, {
+      expiresIn: authConfig.expiresIn,
+    });
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token,
+    });
+  }
+}
+
+export default new SessionController();
