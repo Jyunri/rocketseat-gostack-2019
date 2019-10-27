ddd5e579a33d238eb44264533d3e9133d01f9b40
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-10-27 16:51:58 -0300

    2.3.13

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -27,12 +27,14 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
-routes.get('/notifications', NotificationController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
