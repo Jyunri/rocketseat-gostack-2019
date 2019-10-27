927e9594e5ec699bc71025f94c780466e2d1f0fa
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-10-27 17:07:59 -0300

    2.3.14

--- module02/src/app/controllers/AppointmentController.js
+++ module02/src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -114,6 +114,34 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    // check if user is appointment owner
+    if (appointment.user_id !== req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'You do not permission to cancel this appointment' });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    /**
+     * Check if canceling is being made two hours before the appointment date
+     */
+    if (isBefore(dateWithSub, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
