908df7b6b18161b2d844fa5f5d12b5491e86783c
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-10-29 00:14:06 -0300

    2.3.17

--- module02/src/app/controllers/AppointmentController.js
+++ module02/src/app/controllers/AppointmentController.js
@@ -5,7 +5,9 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -156,21 +158,9 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
-    const formattedDate = format(
-      appointment.date,
-      "'dia' dd 'de' MMMM', às' H:mm'h'",
-      { locale: pt }
-    );
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancelation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: formattedDate,
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
