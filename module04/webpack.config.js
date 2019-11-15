1a9123c79d566bd7ad0e9d4d35505c626d566810
Jonathan Suenaga <nekojimy@gmail.com>
2019-11-15 20:43:12 -0300

    3.1.3

--- module04/webpack.config.js
+++ module04/webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'css-loader' }
+        ]
       }
     ]
   }
