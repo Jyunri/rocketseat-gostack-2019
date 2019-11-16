36d741f080fa35741f9fb682d3f2decee491401a
Jonathan Suenaga <nekojimy@gmail.com>
2019-11-16 01:56:55 -0300

    3.1.7 - 3.1.9

--- module04/src/components/Techlist.js
+++ module04/src/components/Techlist.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import TechItem from './Techitem';
 
 class Techlist extends Component {
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -9,13 +11,40 @@ class Techlist extends Component {
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ techs: [...this.state.techs, this.state.newTech] });
+    this.setState({ newTech: '' });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>ReactNative</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          onChange={this.handleInputChange}
+          type="text"
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
