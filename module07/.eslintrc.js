cf91c315c9b60d6c4a4a1d97fdd032626f1e2d4d
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-07 09:31:32 -0300

    4.1.12

--- module07/.eslintrc.js
+++ module07/.eslintrc.js
@@ -33,5 +33,6 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': [0, "always"],
     'react/static-property-placement': 'off',
+    'no-console': ["error", { allow: ["tron"] }]
   },
 };
