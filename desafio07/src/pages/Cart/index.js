07c8eee5d8328484145427bf1b206ca040fe5e6f
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-11 21:35:48 -0300

    finish styling cart

--- desafio07/src/pages/Cart/index.js
+++ desafio07/src/pages/Cart/index.js
@@ -1,8 +1,97 @@
 import React from 'react';
-import {View} from 'react-native';
 
-// import { Container } from './styles';
+import {RectButton} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  EmptyCartText,
+  ProductList,
+  Product,
+  ProductSKU,
+  ProductImage,
+  ProductDescription,
+  ProductTitle,
+  ProductPrice,
+  ProductSubtotalContainer,
+  ProductAmountContainer,
+  ProductAmountDecrement,
+  ProductAmount,
+  ProductAmountIncrement,
+  ProductSubtotal,
+  ProductTotalContainer,
+  ProductTotalLabel,
+  ProductTotal,
+  FinishOrderButton,
+  FinishOrderButtonText,
+} from './styles';
 
-export default function Cart() {
-  return <View />;
+import {formatPrice} from '../../util/format';
+
+const products = [
+  {
+    id: 1,
+    title: 'Tênis de Caminhada Leve Confortável',
+    price: 179.9,
+    image:
+      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+    price: 139.9,
+    image:
+      'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+  },
+];
+
+function renderItem(item) {
+  return (
+    <Product>
+      <ProductSKU>
+        <ProductImage source={{uri: item.image}} />
+        <ProductDescription>
+          <ProductTitle>{item.title}</ProductTitle>
+          <ProductPrice>{formatPrice(item.price)}</ProductPrice>
+        </ProductDescription>
+      </ProductSKU>
+      <ProductSubtotalContainer>
+        <ProductAmountContainer>
+          <ProductAmountDecrement onClick={() => console.log(item)}>
+            <Icon name="remove-circle" size={25} color="#7159c1" />
+          </ProductAmountDecrement>
+          <ProductAmount>3</ProductAmount>
+          <ProductAmountIncrement onClick={() => console.log(item)}>
+            <Icon name="add-circle" size={25} color="#7159c1" />
+          </ProductAmountIncrement>
+        </ProductAmountContainer>
+        <ProductSubtotal>{formatPrice(item.price)}</ProductSubtotal>
+      </ProductSubtotalContainer>
+    </Product>
+  );
+}
+
+export default function Cart({navigation}) {
+  if (products.length === 0) {
+    return (
+      <Container>
+        <EmptyCartText>Seu Carrinho Está vazio.</EmptyCartText>
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <ProductList
+        data={products}
+        keyExtractor={product => product.id}
+        renderItem={({item}) => renderItem(item)}
+      />
+      <ProductTotalContainer>
+        <ProductTotalLabel>TOTAL</ProductTotalLabel>
+        <ProductTotal>{formatPrice(700)}</ProductTotal>
+      </ProductTotalContainer>
+      <FinishOrderButton onPress={() => navigation.navigate('Home')}>
+        <FinishOrderButtonText>FINALIZAR PEDIDO</FinishOrderButtonText>
+      </FinishOrderButton>
+    </Container>
+  );
 }
---
+++ desafio07/src/pages/Cart/styles.js
@@ -0,0 +1,139 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  margin: 40px 20px;
+  margin-bottom: 100px;
+  padding: 0px 20px;
+
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const EmptyCartText = styled.Text`
+  flex: 1;
+  margin: 30px;
+  color: #000;
+  font-weight: bold;
+  font-size: 30px;
+  text-align: center;
+`;
+
+export const ProductList = styled.FlatList.attrs({
+  showVerticalScrollIndicator: false,
+})`
+  margin-top: 20px;
+`;
+
+export const Product = styled.View`
+  margin: 20px 0px;
+  border-radius: 4px;
+`;
+
+export const ProductSKU = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+  background: #fff;
+`;
+
+export const ProductImage = styled.Image`
+  width: 80px;
+  height: 80px;
+  align-self: center;
+`;
+
+export const ProductDescription = styled.View`
+  margin: 0 10px;
+`;
+
+export const ProductTitle = styled.Text`
+  color: #333;
+  font-size: 20px;
+`;
+
+export const ProductPrice = styled.Text`
+  margin-top: 5px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ProductSubtotalContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 15px;
+  padding-right: 15px;
+  height: 50px;
+  background: #eee;
+`;
+
+export const ProductAmountContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const ProductAmountDecrement = styled(RectButton)`
+  margin: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductAmount = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  height: 40px;
+  width: 80px;
+  background: #fff;
+  border-radius: 4px;
+  padding-right: 15px;
+  border: 1px solid #eee;
+`;
+
+export const ProductAmountIncrement = styled(RectButton)`
+  margin: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProductSubtotal = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ProductTotalContainer = styled.View`
+  margin: 40px 0px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ProductTotalLabel = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #999;
+`;
+export const ProductTotal = styled.Text`
+  margin-top: 20px;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const FinishOrderButton = styled(RectButton)`
+  margin-top: auto;
+  margin-bottom: 10px;
+
+  max-height: 50px;
+  background: #7159c1;
+  flex: 5;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+`;
+
+export const FinishOrderButtonText = styled.Text`
+  flex: 4;
+  flex-direction: row;
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+  text-align: center;
+`;
