21eb8dcfd6a32bd6e4aee0b20438b08a5027e74f
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-14 18:06:00 -0300

    4.2.1-4.2.5

---
+++ module08/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function App() {
+  const [techs, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...techs, newTech]);
+  }, [newTech, techs]);
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTech(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techsSize = useMemo(() => techs.length, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techsSize} tecnologias</strong>
+      <input onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;
