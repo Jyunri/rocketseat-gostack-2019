bc95e8567515826c3f6e8921e71255cc8f795e35
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-21 17:19:25 -0300

    5.1.24

--- module09/src/pages/Profile/index.js
+++ module09/src/pages/Profile/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
