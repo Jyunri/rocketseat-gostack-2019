d4e07a271dfc2c5147f5dad05bbb9c7a47fad83f
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-21 11:26:08 -0300

    5.1.22

--- module09/src/components/Notifications/index.js
+++ module09/src/components/Notifications/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 import { MdNotifications } from 'react-icons/md';
+import { formatDistance, parseISO } from 'date-fns';
+import { pt } from 'date-fns/esm/locale';
 import {
   Container,
   Scroll,
@@ -8,57 +10,73 @@ import {
   NotificationList,
   Notification,
 } from './styles';
+import api from '~/services/api';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: pt }
+        ),
+      }));
+      setNotifications(data);
+    }
+
+    loadNotifications();
+  }, []);
+
+  const hasUnread = useMemo(
+    () => !!notifications.find(notification => notification.read === false),
+    [notifications]
+  );
+
+  function handleToogleVisible() {
+    setVisible(!visible);
+  }
+
+  async function handleMarkAsAdd(id) {
+    // esse endpoint ja seta a notification como lida. A proxima vez que renderizar ele vai estar como lida
+    await api.put(`notifications/${id}`);
+
+    // aqui estamos setando como lida no estado sem precisar refazer a request
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification
+      )
+    );
+  }
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge hasUnread={hasUnread} onClick={handleToogleVisible}>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          <Notification unread>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
-
-          <Notification>
-            <p>Você possui um novo agendamento para amanha</p>
-            <time>há 2 dias atrás</time>
-            <button type="button">Marcar como lida</button>
-          </Notification>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              {!notification.read && (
+                <button
+                  type="button"
+                  onClick={() => handleMarkAsAdd(notification._id)}
+                >
+                  Marcar como lida
+                </button>
+              )}
+            </Notification>
+          ))}
         </Scroll>
       </NotificationList>
     </Container>
