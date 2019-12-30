7cab4ea466b215bdae8071204594db79185fcf89
Jimy Suenaga <jimy@MacBook-Pro-de-Fabiano.local>
2019-12-30 18:26:20 -0300

    5.2.20

--- module10/src/pages/New/SelectProvider/index.js
+++ module10/src/pages/New/SelectProvider/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
 
-export default function SelectProvider() {
-  return <Background />;
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+import api from '~/services/api';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider: item })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: item.avatar
+                    ? item.avatar.url
+                    : `https://api.adorable.io/avatar/50/${item.name}.png`,
+                }}
+              />
+              <Name>{item.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
